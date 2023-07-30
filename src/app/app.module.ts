4666e20327dcf145da56e38c881f30704d83e9d1
orianig <oriana.ig016@gmail.com>
2023-07-30 22:05:49 +0200

    feat: add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesComponent } from './pages/games/games.component';
+import { CardComponent } from './pages/games/components/card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
+  declarations: [AppComponent, GamesComponent, CardComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
