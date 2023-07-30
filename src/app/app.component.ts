b53ab756c62178002dd57026fd205922b8a774d8
orianig <oriana.ig016@gmail.com>
2023-07-30 22:05:29 +0200

    feat: add game service - get game list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { GamesService } from './services/games.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,19 +6,6 @@ import { GamesService } from './services/games.service';
 })
 export class AppComponent {
   title = 'angular-game';
-  gameList: any[] = [];
-
   // inyecto el servicio en el componente
-  constructor(private gameService: GamesService) {
-    this.getGameList();
-  }
-
-  getGameList() {
-    // me subscribo al metodo que me devuelve los juegos
-    this.gameService.getAllGames().subscribe((results) => {
-      console.log(results);
-      // asigno los juegos a la variable gameList
-      this.gameList = results;
-    });
-  }
+  constructor() {}
 }
